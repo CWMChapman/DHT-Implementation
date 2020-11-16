441f245c28f5d3b6ddd597ab7e255390ac4d4d93
Christopher Chapman <cwmchapman@gmail.com>
2020-11-16 01:08:28 -0500

    this version has the encode decode for the 72 bit message format -- example code commented out

--- src/client.cpp
+++ src/client.cpp
@@ -27,46 +27,45 @@ decode sections
 int decode_9byte(std::array<uint8_t, 9> *message, int section) {
 	int array_index = 0; // remember that the array is of lenght 4, not 4 * 8.
 	int bit_index = 0;	 // which bit within each element of the array we are refering to
-	int action = 0;		 // message's action	-- section 0
-	int key = 0;		 // message's key	-- section 1
-	int value = 0;		 // message's value	-- section 2
-
-
-	for (int i = 0; i < 8; ++i) {
-		action |= (((*message)[array_index] >> (bit_index)) & 1) << (7-i);
-		++bit_index;
-	}
-	++array_index;
-	for (int i = 8; i < 40; ++i) {
-		key |= (((*message)[array_index] >> bit_index) & 1) << (31 + 8 - i);
-
-		if ((i + 1) % 8 == 0) {
-			++array_index;
-			bit_index = 0;
-		}
-		else {
-			++bit_index;
-		}
-	}
-	for (int i = 40; i < 72; ++i) {
-		value |= (((*message)[array_index] >> bit_index) & 1) << (31 + 40 - i);
-
-		if ((i + 1) % 8 == 0) {
-			++array_index;
-			bit_index = 0;
-		}
-		else {
-			++bit_index;
-		}
-	}
-
+	
 	if (section == 0) {
+		int action = 0;		 // message's action	-- section 0
+		for (int i = 0; i < 8; ++i) {
+			action |= (((*message)[array_index] >> (bit_index)) & 1) << (7-i);
+			++bit_index;
+		}
 		return action;
 	}
 	else if (section == 1) {
+		int key = 0;		 // message's key	-- section 1
+		array_index = 1;
+		for (int i = 8; i < 40; ++i) {
+			key |= (((*message)[array_index] >> bit_index) & 1) << (31 + 8 - i);
+
+			if ((i + 1) % 8 == 0) {
+				++array_index;
+				bit_index = 0;
+			}
+			else {
+				++bit_index;
+			}
+		}
 		return key;
 	}
 	else if (section == 2) {
+		int value = 0;		 // message's value	-- section 2
+		array_index = 5;
+		for (int i = 40; i < 72; ++i) {
+			value |= (((*message)[array_index] >> bit_index) & 1) << (31 + 40 - i);
+
+			if ((i + 1) % 8 == 0) {
+				++array_index;
+				bit_index = 0;
+			}
+			else {
+				++bit_index;
+			}
+		}
 		return value;
 	}
 	else {
@@ -79,26 +78,10 @@ void encode_9byte(std::array<uint8_t, 9> *message, int action, int key, int valu
 	int array_index = 0; // remember that the array is of lenght 4, not 4 * 8.
 	int bit_index = 0;	 // which bit within each element of the array we are refering to
 	
-
-	std::cout << "action: " << std::endl;
-
-	for (int i = 0; i < 32; ++i) {
-		std::cout << ((action >> i) & 1);
-		if ((i+1)%8 == 0) 
-			std::cout << " ";
-	}
-	std::cout << std::endl;
-
-	// for (int i = 0; i < 32; ++i) {
-	// 	action &= ~(1 << i);
-	// }
 	for (int i = 0; i < 8; ++i) {
-		std::cout << ((action >> i) & 1);
 		(*message)[array_index] |= ((action >> i) & 1) << (7-bit_index);
-		// std::cout << (((*message)[array_index] >> bit_index) & 1);
 		++bit_index;
 	}
-	std::cout << std::endl;
 	++array_index;
 	for (int i = 8; i < 40; ++i) {
 		(*message)[array_index] &= ~(1 << bit_index);
@@ -124,26 +107,6 @@ void encode_9byte(std::array<uint8_t, 9> *message, int action, int key, int valu
 			++bit_index;
 		}
 	}
-	
-	
-
-	array_index = 0;
-	bit_index = 0;
-	std::cout << array_index << ": ";
-	for (int i = 0; i < 72; ++i) {
-		std::cout << (((*message)[array_index] >> bit_index) & 1);
-		if ((i + 1) % 8 == 0) {
-			++array_index;
-			bit_index = 0;
-			if (array_index != 9)
-				std::cout << "   " << array_index << ": ";
-		}
-		else {
-			++bit_index;
-		}
-	}
-	std::cout << std::endl;
-
 	return;
 }
 
@@ -155,17 +118,17 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	/* EXAMPLE ENCODE DECODE USAGE
 	std::array<uint8_t, 9> message;
 	int a = 59;
 	int k = 4321;
 	int v = 1234;
-	std::cout << "Action: " << a << ", Key: " << k << ", Value: " << v << std::endl;
+	std::cout << "Before: \tAction: " << a << ", Key: " << k << ", Value: " << v << std::endl;
 	encode_9byte(&message, a, k, v);
 	int action = decode_9byte(&message, 0);
 	int key = decode_9byte(&message, 1);
 	int value = decode_9byte(&message, 2);
-	std::cout << "Action: " << action << ", Key: " << key << ", Value: " << value << std::endl;
-	/* EXAMPLE ENCODE DECODE USAGE
+	std::cout << "After: \t\tAction: " << action << ", Key: " << key << ", Value: " << value << std::endl;
 	*/
 
 	asio::io_context io_context;
